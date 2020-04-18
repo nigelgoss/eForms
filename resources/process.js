fa1634095e935763e2794b1063317652e440e296
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-04-18 23:31:20 +0100

    Create process.js

---
+++ resources/process.js
@@ -0,0 +1,44 @@
+(function () {
+
+	if (!NodeList.prototype.forEach) { NodeList.prototype.forEach = Array.prototype.forEach; };
+			
+    var disabled = false;
+
+    window.process = function () {
+
+      if (disabled === true) return;
+      disabled = true;
+
+      var $d = {};
+      document.querySelectorAll("*[name]").forEach(function ($v) {
+        $d[$v.name] = ($v.value === "") ? null : $v.value;
+      });
+
+	$e = validate();
+      if ($e.length > 0) {
+
+        alert("Errors:\n- " + $e.join("\n- "));
+        disabled = false;
+
+      } else {
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState !== 4) return;
+          disabled = false;
+          if (xhr.status !== 200) {
+            alert("There has been an error with the submission. Please retry.");
+          } else {
+            location.href = ".";
+          };
+        };
+        xhr.open("GET", ".", true);
+        xhr.send();
+
+        console.log(JSON.stringify([location.hash.split("#")[1], $d]));
+
+      };
+
+    };
+    
+}());
