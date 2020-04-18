32deb99af7f29a30e7710bc0bfaca107830b3dd8
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-04-18 23:45:19 +0100

    Update process.js

--- resources/process.js
+++ resources/process.js
@@ -1,44 +1,50 @@
 (function () {
 
 	if (!NodeList.prototype.forEach) { NodeList.prototype.forEach = Array.prototype.forEach; };
-			
-    var disabled = false;
 
-    window.process = function () {
+	var disabled = false;
 
-      if (disabled === true) return;
-      disabled = true;
+	window.process = function () {
 
-      var $d = {};
-      document.querySelectorAll("*[name]").forEach(function ($v) {
-        $d[$v.name] = ($v.value === "") ? null : $v.value;
-      });
+		if (disabled === true) return;
+		disabled = true;
 
-	$e = validate();
-      if ($e.length > 0) {
+		var $d = {};
+		document.querySelectorAll("*[name]").forEach(function ($v) {
+			if ($d[$v.name] === undefined) $d[$v.name] = ($v.type === "checkbox") ? [] : null;
+			if ($v.checked === false) return;
+			if ($v.checked === true) {
+				$d[$v.name].push($v.value);
+			} else {
+				$d[$v.name] = ($v.value === "") ? null : $v.value;
+			};
+		});
 
-        alert("Errors:\n- " + $e.join("\n- "));
-        disabled = false;
+		var $e = validate($d);
+		if ($e.length > 0) {
 
-      } else {
+			alert("Errors:\n- " + $e.join("\n- "));
+			disabled = false;
 
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-          if (xhr.readyState !== 4) return;
-          disabled = false;
-          if (xhr.status !== 200) {
-            alert("There has been an error with the submission. Please retry.");
-          } else {
-            location.href = ".";
-          };
-        };
-        xhr.open("GET", ".", true);
-        xhr.send();
+		} else {
 
-        console.log(JSON.stringify([location.hash.split("#")[1], $d]));
+			var xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState !== 4) return;
+				disabled = false;
+				if (xhr.status !== 200) {
+					alert("There has been an error with the submission. Please retry.");
+				} else {
+					location.href = ".";
+				};
+			};
+			xhr.open("GET", ".", true);
+			xhr.send();
 
-      };
+			console.log(JSON.stringify([location.hash.split("#")[1], $d]));
 
-    };
-    
+		};
+
+	};
+
 }());
